74e776801181ae4f92b7a985a3bd1c9673cda90b
Anthony Fielding <ant@antfie.com>
2024-08-29 10:13:23 +0100

    Encode logs using %enc{%m}{CRLF}

--- src/main/java/com/veracode/afielding/example/ExampleController.java
+++ src/main/java/com/veracode/afielding/example/ExampleController.java
@@ -3,19 +3,19 @@ package com.veracode.afielding.example;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import java.lang.Throwable;
-import java.util.regex.Matcher;
+
 import java.util.regex.Pattern;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 import org.owasp.encoder.Encode;
 
 @RestController
 public class ExampleController {
 	private static final String ACCOUNT_NUMBER_VALIDATION_PATTERN = "^\\d{8}$";
-	private static final Logger logger = LoggerFactory.getLogger(ExampleController.class.getName());
+	private static final Logger logger = LogManager.getLogger(ExampleController.class);
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String index(String untrusted) {
